5e83639907e5c1e32717229e4b63d212134044f6
viru0909-dev <gadekarvirendra4@gmail.com>
2025-11-29 04:20:59 +0530

    added pom.xml and sturcture to backend

---
+++ backend/case-service/src/main/java/com/nyaysetu/caseservice/CaseServiceApplication.java
@@ -0,0 +1,11 @@
+package com.nyaysetu.caseservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CaseServiceApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(CaseServiceApplication.class, args);
+    }
+}
---
+++ backend/user-verification-service/src/main/java/com/nyaysetu/userverificationservice/UserVerificationServiceApplication.java
@@ -0,0 +1,11 @@
+package com.nyaysetu.userverificationservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class UserVerificationServiceApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(UserVerificationServiceApplication.class, args);
+    }
+}
